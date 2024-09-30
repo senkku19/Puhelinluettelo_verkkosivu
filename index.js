de351e17f8f41402ffe5ca3d7ff6b808e06464ea
senkku19 <senjamoilanen@gmail.com>
2024-09-30 17:22:06 +0300

    Part 3.15-3.18.

--- index.js
+++ index.js
@@ -12,29 +12,6 @@ app.use(express.static('dist'))
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 
-/*let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"  
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]*/
-
 app.get('/info', (request, response) => {
     const timeStamp = new Date()
     const entryCount = persons.length
@@ -53,17 +30,24 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request,response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request,response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person){
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request,response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -87,14 +71,6 @@ app.post('/api/persons', morganPost, (request, response) => {
         })
     } 
 
-    /*const nameFound = persons.find(person => person.name === body.name)
-
-    if (nameFound){
-        return response.status(400).json({
-            error: 'name is already in the phonebook'
-        })
-    }*/
-
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -104,6 +80,33 @@ app.post('/api/persons', morganPost, (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response,next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
